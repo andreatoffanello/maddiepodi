b35cf8aa815cadf8f66fa75e846e99dadcbd8559
Andrea Toffanello <andrea.toffanello@gmail.com>
2019-12-03 14:36:31 +0100

    fix safari scroll problem

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,4 +6,26 @@ $(".nav a").click( function() {
 });
 
 
+$('a[href^="#"]').on('click', function(event) {
+  var hash = '#' + $(this).attr('href').split('#')[1]
+  var element = $(hash)
+  if (element.length) {
+    event.preventDefault();
+    history.pushState(hash, undefined, hash)
+    $('html, body').animate({scrollTop: element.offset().top}, 500)
+  }
+});
+
+window.addEventListener('popstate', function(e) {
+  if(e.state && e.state.startsWith('#') && $(e.state).length){
+    $('html, body').animate({scrollTop: $(e.state).offset().top}, 500)
+  }
+});
+
+$('html, body').on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+  $('html, body').stop();
+});
+
+
+
 });
